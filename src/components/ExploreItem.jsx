d066ce4565625ef0193f47ffa60cc21847f305cb
peter <g2649532831@gmail.com>
2023-10-08 04:47:14 -0400

    Expore component (needs to be worked on)

---
+++ src/components/ExploreItem.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "../style/ExploreItem.css";
+const ExploreItem = () => {
+  return (
+
+      <div className="image-container">
+                <img src="src/assets/dummymap.png" alt="Square" className="square-image" />
+                <div className="description">
+                    Log Island
+
+                </div>
+      </div>
+  );
+};
+
+export default ExploreItem;

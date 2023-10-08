154c5c5e58e26f88d858413f30e0274338cd4537
J <59581793+daechi73@users.noreply.github.com>
2023-10-08 00:42:24 -0400

    fixed class to className

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,27 +5,27 @@ const Nav = () => {
       <ul>
         <li>
           <div className="nav-home-icon container-nav-icon">
-            <i class="fa fa-home nav-icon" aria-hidden="true"></i>
+            <i className="fa fa-home nav-icon" aria-hidden="true"></i>
           </div>
           <div className="nav-title">Home</div>
         </li>
         <li>
           <div className="nav-map-icon container-nav-icon">
-            <i class="fa fa-map-marker nav-icon" aria-hidden="true"></i>
+            <i className="fa fa-map-marker nav-icon" aria-hidden="true"></i>
           </div>
 
           <div className="nav-title">Explore</div>
         </li>
         <li>
           <div className="nav-community-icon container-nav-icon">
-            <i class="fa fa-comments-o nav-icon" aria-hidden="true"></i>
+            <i className="fa fa-comments-o nav-icon" aria-hidden="true"></i>
           </div>
 
           <div className="nav-title">Community</div>
         </li>
         <li>
           <div className="nav-profile-icon container-nav-icon">
-            <i class="fa fa-user nav-icon fa-1x" aria-hidden="true"></i>
+            <i className="fa fa-user nav-icon fa-1x" aria-hidden="true"></i>
           </div>
           <div className="nav-title">Profile</div>
         </li>
